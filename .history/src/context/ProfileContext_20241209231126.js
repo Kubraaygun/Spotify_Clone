74a1f2c2bda03a051b4509cf26018c446e3872c4
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-12-09 23:12:17 +0300

    Add ProfileContext

---
+++ .history/src/context/ProfileContext_20241209231126.js
@@ -0,0 +1,13 @@
+import {createContext} from 'react';
+
+export const ProfileContext = createContext();
+
+export const ProfileProvider = ({children}) => {
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  return (
+    <ProfileContext.Provider value={{}}>{children}</ProfileContext.Provider>
+  );
+};
---
+++ .history/src/context/ProfileContext_20241209231136.js
@@ -0,0 +1,15 @@
+import {createContext} from 'react';
+
+export const ProfileContext = createContext();
+
+export const ProfileProvider = ({children}) => {
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  return (
+    <ProfileContext.Provider value={{profileData, loading, error}}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
